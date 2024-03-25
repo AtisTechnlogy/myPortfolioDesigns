caab25f3a5d52561c13df618cbc7071805d73181
sitaram <atistechnlogy@gmail.com>
2024-03-25 10:37:41 +0530

    Website customised
    my Images & projects are added

--- myPortfolioSite/scripts/main.js
+++ myPortfolioSite/scripts/main.js
@@ -4,3 +4,12 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+function comingSoonOne() {
+     document.getElementById("demo1").innerHTML = "Under Construction";
+}
+function comingSoonTwo() {
+     document.getElementById("demo2").innerHTML = "Under Construction";
+}
+function comingSoonThree() {
+     document.getElementById("demo3").innerHTML = "Under Construction";
+}
